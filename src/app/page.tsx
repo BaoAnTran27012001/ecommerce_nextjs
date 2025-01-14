6e60ae8f27851105e6354adf9789a332c03a5e40
Trần Bảo An <worldcoder1234@gmail.com>
2025-01-14 19:48:55 +0700

    Complete Setup UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import ThemeToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-      <h1 className="text-red-500">Welcome to the course</h1>
+    <div className="p-5">
+      <div className="w-100 flex justify-end">
+        <ThemeToggle />
+      </div>
+      <h1 className="text-red-500 font-poppins">Welcome to the course</h1>
+      <Button variant="destructive">Click Here</Button>
     </div>
   );
 }
